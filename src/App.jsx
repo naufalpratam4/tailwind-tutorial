71c909395c59573d6ff845c72dc5c3e5c32257c8
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-26 22:23:11 +0700

    add vite

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        perferendis praesentium cupiditate autem hic nulla ipsum sequi
+        repellendus et! Sunt quibusdam harum perferendis maxime officia ducimus
+        officiis obcaecati sit sequi!
+      </h2>
+    </>
+  );
+}
+
+export default App;
