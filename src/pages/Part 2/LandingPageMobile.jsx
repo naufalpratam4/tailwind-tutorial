219c1fe1ef69763657fa3d6546bbfe4bbbb49f07
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-27 23:45:47 +0700

    succesfull responsive website

--- src/pages/Part 2/LandingPageMobile.jsx
+++ src/pages/Part 2/LandingPageMobile.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import NavBar from "../../components/Part 2/navbar/NavBar";
+import Hero from "../../components/Part 2/hero/Hero";
 
 function LandingPageMobile() {
-  return <div>LandingPageMobile</div>;
+  return (
+    <>
+      {" "}
+      <div className="md:mx-auto md:px-32 md:py-4 p-4">
+        <NavBar />
+        <Hero />
+      </div>
+    </>
+  );
 }
 
 export default LandingPageMobile;
